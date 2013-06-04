ebc85d489fc0995014f62ef6b392995b1b6f6908
Sergey D <sclinede@gmail.com>
2013-06-04 17:57:48 +0600

    Changed tests and validator

--- spec/validators/uri_validator_spec.rb
+++ spec/validators/uri_validator_spec.rb
@@ -2,16 +2,15 @@ require 'spec_helper'
 require 'validators/uri_validator' 
 
 describe UriValidator do
-
-  before(:each) do
-    @validator = UriValidator.new({:attributes => {}})
-    @mock = mock('model')
-    @mock.stub("errors").and_return([])
-    @mock.errors.stub('[]').and_return({})  
-    @mock.errors[].stub('<<')
-  end
   
+  before(:all) { class TestModel; include ActiveModel::Validations; end }
+
+  let(:validator) { UriValidator.new({:attributes => {}}) }
+
+  let(:record) { TestModel.new }
+
   let(:attribute) { "uri" }
+
   let(:correct_uri) { "http://ya.ru" }
   let(:wrong_scheme_uri) { "https://ya.ru" }
   let(:wrong_uri) { "www.ya.ru" }
@@ -20,33 +19,34 @@ describe UriValidator do
   let(:not_200_code_uri) { "http://www.gismeteo.ru/city/daily/4517/abc.jpg" }
 
   it 'should be valid if correct URI passed' do
-    @mock.should_not_receive('errors')
-    @validator.validate_each(@mock, attribute, correct_uri)
+
+    validator.validate_each(record, attribute, correct_uri)
+    record.errors.empty?.should be_true
   end
 
   it 'should not be valid if wrong URI was passed' do
-    @mock.errors[].should_receive("<<").with("Wrong URI format")
-    @validator.validate_each(@mock, attribute, wrong_uri)
+    validator.validate_each(record, attribute, wrong_uri)
+    record.errors[attribute].should == ["Wrong URI format"]
   end
 
   it 'should not be valid if wrong scheme in URI was passed' do
-    @mock.errors[].should_receive("<<").with("Wrong URI format")
-    @validator.validate_each(@mock, attribute, wrong_scheme_uri)
+    validator.validate_each(record, attribute, wrong_scheme_uri)
+    record.errors[attribute].should == ["Wrong URI format"]
   end
 
   it 'should not be valid if requesting URI timeout error' do
-    @mock.errors[].should_receive("<<").with("URI request timeout error (> 3 sec.)")
-    @validator.validate_each(@mock, attribute, timeout_error_uri)
+    validator.validate_each(record, attribute, timeout_error_uri)
+    record.errors[attribute].should == ["URI request timeout error (> 3 sec.)"]
   end
 
   it 'should not be valid if host invalid' do
-    @mock.errors[].should_receive("<<").with("Host unavailable")
-    @validator.validate_each(@mock, attribute, socket_error_uri)
+    validator.validate_each(record, attribute, socket_error_uri)
+    record.errors[attribute].should == ["Host unavailable"]
   end
 
   it 'should not be valid if response code not 200 OK' do
-    @mock.errors[].should_receive("<<").with("Error happend during requesting URI")
-    @validator.validate_each(@mock, attribute, not_200_code_uri)
+    validator.validate_each(record, attribute, not_200_code_uri)
+    record.errors[attribute].should == ["Error happend during requesting URI"]
   end
 
 end
