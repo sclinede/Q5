4cedbee917d9feecd5df8f152c925b27852cbd37
Sergey D <sclinede@gmail.com>
2013-06-04 17:05:07 +0600

    Changed tests and validator

--- lib/validators/uri_validator.rb
+++ lib/validators/uri_validator.rb
@@ -13,21 +13,12 @@ class UriValidator < ActiveModel::EachValidator
       
       response = resource.head
      
-      case response.code  
-      when 200
-        true
-      when 408
-        record.errors[attribute] << "URI request timeout error (> 3 sec.) [server respond]"      
-      else
-        record.errors[attribute] << "URI response not 200 OK"
-      end
-
-    rescue RestClient::ResourceNotFound
-      record.errors[attribute] << "Resource not found error"
+      record.errors[attribute] << "URI response not 200 OK" unless response.code == 200  
+    
     rescue RestClient::RequestTimeout
-      record.errors[attribute] << "URI request timeout error (> 3 sec.) [open connection]"
+      record.errors[attribute] << "URI request timeout error (> 3 sec.)"
     rescue RestClient::Exception
-      record.errors[attribute] << "Error happend during requesting URI [connection]"
+      record.errors[attribute] << "Error happend during requesting URI"
     rescue SocketError
       record.errors[attribute] << "Host unavailable"
     end
--- spec/validators/uri_validator_spec.rb
+++ spec/validators/uri_validator_spec.rb
@@ -17,9 +17,7 @@ describe UriValidator do
   let(:wrong_uri) { "www.ya.ru" }
   let(:timeout_error_uri) { "http://ya.ru:889" }
   let(:socket_error_uri) { "http://yaefb.ru" }
-  let(:no_resource_uri) { "http://www.gismeteo.ru/city/daily/4517/abc.jpg" }
-  let(:server_response_timout_uri) { "http://ya.ru" }
-  let(:not_200_code_uri) { "http://ya.ru" }
+  let(:not_200_code_uri) { "http://www.gismeteo.ru/city/daily/4517/abc.jpg" }
 
   it 'should be valid if correct URI passed' do
     @mock.should_not_receive('errors')
@@ -37,7 +35,7 @@ describe UriValidator do
   end
 
   it 'should not be valid if requesting URI timeout error' do
-    @mock.errors[].should_receive("<<").with("URI request timeout error (> 3 sec.) [open connection]")
+    @mock.errors[].should_receive("<<").with("URI request timeout error (> 3 sec.)")
     @validator.validate_each(@mock, attribute, timeout_error_uri)
   end
 
@@ -46,19 +44,9 @@ describe UriValidator do
     @validator.validate_each(@mock, attribute, socket_error_uri)
   end
 
-  it 'should not be valid if resource not found' do
-    @mock.errors[].should_receive("<<").with("Resource not found error")
-    @validator.validate_each(@mock, attribute, no_resource_uri)
-  end
-
   it 'should not be valid if response code not 200 OK' do
-    @mock.errors[].should_receive("<<").with("URI response not 200 OK")
+    @mock.errors[].should_receive("<<").with("Error happend during requesting URI")
     @validator.validate_each(@mock, attribute, not_200_code_uri)
   end
 
-  it 'should not be valid if server response timeout' do
-    @mock.errors[].should_receive("<<").with("URI request timeout error (> 3 sec.) [server respond]")
-    @validator.validate_each(@mock, attribute, server_response_timout_uri)
-  end
- 
 end
